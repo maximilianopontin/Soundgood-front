ec6b679753c0ce7093dcc6691bbd5f2feb6f94e8
LudmilaMiguens <miguensludmila@gmail.com>
2024-11-01 20:07:39 -0300

    estilos en premium

--- src/Componentes/Cuenta/Premium.jsx
+++ src/Componentes/Cuenta/Premium.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import Footer from "../Footer/Footer";
 import { Nav } from "../Nav/Nav";
-import { Link } from 'react-router-dom';
 import './EditarPerfil.css';
-import { initMercadoPago, Wallet } from '@mercadopago/sdk-react'
+import { initMercadoPago, Wallet } from '@mercadopago/sdk-react';
+import './premium.css'
 
 function Premium() {
-    const [preferendeId, setPreferenceId] = useState(null)
-    initMercadoPago('APP_USR-7481233767070694-102420-be7e374961dd92e3cc39446b697d1e19-225509543', { locale: 'es-AR' })
+    const [preferendeId, setPreferenceId] = useState(null);
+    initMercadoPago('APP_USR-7481233767070694-102420-be7e374961dd92e3cc39446b697d1e19-225509543', { locale: 'es-AR' });
+    
     const createPreference = async () => {
         try {
             const res = await fetch('https://soundgood-back.onrender.com/create-preference', {
@@ -20,30 +21,36 @@ function Premium() {
                     quantity: 1,
                     unit_price: 50
                 })
-            })
-            const parsed = await res.json()
-            const { id } = parsed
-            return id
+            });
+            const parsed = await res.json();
+            const { id } = parsed;
+            return id;
         } catch (error) {
-            console.error(error.message)
+            console.error(error.message);
         }
-    }
+    };
+
     const handleBuyingProcess = async () => {
-        const id = await createPreference()
-        if (id) setPreferenceId(id)
-    }
-return(
-    <>
-    <Nav/>
-     <main><h2>Sound Good Premium </h2>
-            <button onClick={handleBuyingProcess} >Pagar con mercado pago</button>
-            {
-                preferendeId &&
-                <Wallet initialization={{ preferenceId: preferendeId }} customization={{ texts: { valueProp: 'smart_option' } }} />
-            }
-        </main>
-        <Footer/>
-    </>
-)
- }
- export default Premium;
+        const id = await createPreference();
+        if (id) setPreferenceId(id);
+    };
+
+    return (
+        <>
+            <Nav className="nav-fixed" />
+            <main className="premium-main">
+                <h2 className="premium-title">Sound Good Premium</h2>
+                <button className="premium-button" onClick={handleBuyingProcess}>Pagar con Mercado Pago</button>
+                {preferendeId && (
+                    <Wallet 
+                        initialization={{ preferenceId: preferendeId }} 
+                        customization={{ texts: { valueProp: 'smart_option' } }} 
+                    />
+                )}
+            </main>
+            <Footer className="footer-fixed" />
+        </>
+    );
+}
+
+export default Premium;
